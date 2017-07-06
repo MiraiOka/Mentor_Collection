7fb622bcef0d18f0b901f501fa205b11aaca0863
岡実来 <MIRAI@MacBook-Pro-3.local>
2017-07-07 08:39:43 +0900

    Modify

--- Assets/_MyAssets/Scripts/MentorPurchaseCell.cs
+++ Assets/_MyAssets/Scripts/MentorPurchaseCell.cs
@@ -42,8 +42,7 @@ public class MentorPurchaseCell : MonoBehaviour
 				SoldView();
 				var chara = user.NewCharacter(characterData);
 				PortrateUIManager.instance.MentorTrainingView.AddCharacter(chara);
-
-				// 追記
+				AvatarManager.instance.SpawnAvatar(chara);
 				PopupManager.instance.OpenCommon(characterData.Name+"が入社しました！");
 			});
 
